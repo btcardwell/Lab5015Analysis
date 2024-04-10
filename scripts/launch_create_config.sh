23a3361fe481ac6295a0509116d000756d9420f6
[Reshma] <[pct3kd@virginia.edu]>
2024-04-10 17:39:18 +0200

    Changed the legend

--- scripts/launch_create_config.sh
+++ scripts/launch_create_config.sh
@@ -55,10 +55,10 @@
 
 #config 58.00
 python create_config_TOFHIR2C.py     -t 5     -ml HPK_nonIrr_LYSO820    -c config_58.00    -ov 1.0        -r  6175  --extraLabel angle52
-#python create_config_TOFHIR2C.py     -t 5     -ml HPK_nonIrr_LYSO820    -c config_58.00    -ov 1.25       -r  6179  --extraLabel angle52
-#python create_config_TOFHIR2C.py     -t 5     -ml HPK_nonIrr_LYSO820    -c config_58.00    -ov 0.8        -r  6186  --extraLabel angle52
-#python create_config_TOFHIR2C.py     -t 5     -ml HPK_nonIrr_LYSO820    -c config_58.00    -ov 0.6        -r  6188  --extraLabel angle52
-#python create_config_TOFHIR2C.py     -t 5     -ml HPK_nonIrr_LYSO820    -c config_58.00    -ov 1.5        -r  6189  --extraLabel angle52 # also contains 2.0; plan to add by hand
+python create_config_TOFHIR2C.py     -t 5     -ml HPK_nonIrr_LYSO820    -c config_58.00    -ov 1.25       -r  6179  --extraLabel angle52
+python create_config_TOFHIR2C.py     -t 5     -ml HPK_nonIrr_LYSO820    -c config_58.00    -ov 0.8        -r  6186  --extraLabel angle52
+python create_config_TOFHIR2C.py     -t 5     -ml HPK_nonIrr_LYSO820    -c config_58.00    -ov 0.6        -r  6188  --extraLabel angle52
+python create_config_TOFHIR2C.py     -t 5     -ml HPK_nonIrr_LYSO820    -c config_58.00    -ov 1.5        -r  6189  --extraLabel angle52 # also contains 2.0; plan to add by hand
 #python create_config_TOFHIR2C.py     -t 5     -ml HPK_nonIrr_LYSO820    -c config_58.00    -ov 3.5        -r  6190,6191  --extraLabel angle52
 
 #config 59.00
